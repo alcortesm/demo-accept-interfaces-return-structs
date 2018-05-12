4b7a01b687a45eb0722c0ccd847730952cd80e55
Alberto Cortés <alcortesm@gmail.com>
2018-05-12 20:09:37 +0200

    myhttp4

---
+++ myhttp4/client.go
@@ -0,0 +1,57 @@
+package myhttp4
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func NewClient(inner HTTPClient, contentType string) *Client {
+	return &Client{
+		inner:       inner,
+		contentType: contentType,
+	}
+}
+
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+	Get(url string) (resp *http.Response, err error)
+	Head(url string) (resp *http.Response, err error)
+	Post(url string, contentType string, body io.Reader) (resp *http.Response, err error)
+	PostForm(url string, data url.Values) (resp *http.Response, err error)
+}
+
+type Client struct {
+	inner       HTTPClient
+	contentType string
+}
+
+func (c *Client) Get(url string) (string, error) {
+	resp, err := c.inner.Get(url)
+	if err != nil {
+		return "", err
+	}
+	return readBody(resp)
+}
+
+func (c *Client) Post(url string, body io.Reader) (string, error) {
+	resp, err := c.inner.Post(url, c.contentType, body)
+	if err != nil {
+		return "", err
+	}
+	return readBody(resp)
+}
+
+func readBody(resp *http.Response) (string, error) {
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		err = errors.New(resp.Status)
+	}
+	return string(b), err
+}
---
+++ myhttp4/client_test.go
@@ -0,0 +1,213 @@
+package myhttp4_test
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"local/demo-accept-interfaces-return-structs/myhttp4"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+)
+
+type mock struct {
+	t    *testing.T
+	get  func(url string) (*http.Response, error)
+	post func(url, contentType string, body io.Reader) (*http.Response, error)
+}
+
+func (m *mock) Do(_ *http.Request) (*http.Response, error) {
+	m.t.Fatal("unexpected call to Do")
+	return nil, nil
+}
+
+func (m *mock) Get(url string) (*http.Response, error) {
+	if m.get == nil {
+		m.t.Fatal("unexpected call to Get")
+	}
+	return m.get(url)
+}
+
+func (m *mock) Head(_ string) (*http.Response, error) {
+	m.t.Fatal("unexpected call to Head")
+	return nil, nil
+}
+
+func (m *mock) Post(url, contentType string, body io.Reader) (*http.Response, error) {
+	if m.post == nil {
+		m.t.Fatal("unexpected call to Post")
+	}
+	return m.post(url, contentType, body)
+}
+
+func (m *mock) PostForm(_ string, _ url.Values) (*http.Response, error) {
+	m.t.Fatal("unexpected call to PostForm")
+	return nil, nil
+}
+
+func TestGet(t *testing.T) {
+	t.Parallel()
+
+	urlFix := "url fixture"
+	bodyFix := "body fixture"
+	errFix := errors.New("error fixture")
+
+	for _, tt := range []struct {
+		desc          string
+		want          string         // expected return value
+		wantError     error          // expected return error
+		innerResponse *http.Response // to be returned by the inner client
+		innerError    error          // to be returned by the inner client
+	}{
+		{
+			desc:      "200",
+			want:      bodyFix,
+			wantError: nil,
+			innerResponse: &http.Response{
+				StatusCode: http.StatusOK,
+				Status:     http.StatusText(http.StatusOK),
+				Body:       ioutil.NopCloser(strings.NewReader(bodyFix)),
+			},
+			innerError: nil,
+		},
+		{
+			desc:      "404",
+			want:      bodyFix,
+			wantError: errors.New(http.StatusText(http.StatusNotFound)),
+			innerResponse: &http.Response{
+				StatusCode: http.StatusNotFound,
+				Status:     http.StatusText(http.StatusNotFound),
+				Body:       ioutil.NopCloser(strings.NewReader(bodyFix)),
+			},
+			innerError: nil,
+		},
+		{
+			desc:          "error",
+			want:          "",
+			wantError:     errFix,
+			innerResponse: nil,
+			innerError:    errFix,
+		},
+	} {
+		t.Run(tt.desc, func(t *testing.T) {
+			// mock an HTTPClient to return innerResponse and innerError
+			// to a GET request for the urlFix
+			inner := &mock{
+				t: t,
+				get: func(url string) (*http.Response, error) {
+					if url != urlFix {
+						t.Errorf("wrong url, want %q, got %q", urlFix, url)
+					}
+					return tt.innerResponse, tt.innerError
+				},
+			}
+
+			sut := myhttp4.NewClient(inner, "irrelevant")
+			got, err := sut.Get(urlFix)
+			if !equalError(err, tt.wantError) {
+				t.Errorf("wrong error: want %q, got %q", tt.wantError, err)
+			}
+			if got != tt.want {
+				t.Errorf("want %q, got %q", tt.want, got)
+			}
+		})
+	}
+}
+
+func equalError(a, b error) bool {
+	switch {
+	case a == nil && b == nil:
+		return true
+	case a == nil && b != nil:
+		return false
+	case a != nil && b == nil:
+		return false
+	default:
+		return strings.Contains(a.Error(), b.Error())
+	}
+}
+
+func TestPost(t *testing.T) {
+	t.Parallel()
+
+	const urlFix = "url fixture"
+	const contentTypeFix = "contentType fixture"
+	const reqBodyFix = "request body fixture"
+	const respBodyFix = "response body fixture"
+	errFix := errors.New("error fixture")
+
+	for _, tt := range []struct {
+		desc          string
+		want          string         // expected return value
+		wantError     error          // expected returned error
+		innerResponse *http.Response // to be returned by the inner client
+		innerError    error          // to be returned by the inner client
+	}{
+		{
+			desc:      "200",
+			want:      respBodyFix,
+			wantError: nil,
+			innerResponse: &http.Response{
+				StatusCode: http.StatusOK,
+				Status:     http.StatusText(http.StatusOK),
+				Body:       ioutil.NopCloser(strings.NewReader(respBodyFix)),
+			},
+			innerError: nil,
+		},
+		{
+			desc:      "404",
+			want:      respBodyFix,
+			wantError: errors.New(http.StatusText(http.StatusNotFound)),
+			innerResponse: &http.Response{
+				StatusCode: http.StatusNotFound,
+				Status:     http.StatusText(http.StatusNotFound),
+				Body:       ioutil.NopCloser(strings.NewReader(respBodyFix)),
+			},
+			innerError: nil,
+		},
+		{
+			desc:          "error",
+			want:          "",
+			wantError:     errFix,
+			innerResponse: nil,
+			innerError:    errFix,
+		},
+	} {
+		t.Run(tt.desc, func(t *testing.T) {
+			// mock an HTTPClient to return innerResponse and innerError
+			// to a POST request for the urlFix
+			inner := &mock{
+				t: t,
+				post: func(url, contentType string, body io.Reader) (
+					*http.Response, error) {
+					if url != urlFix {
+						t.Errorf("wrong url, want %q, got %q", urlFix, url)
+					}
+					if contentType != contentTypeFix {
+						t.Errorf("wrong contentType, want %q, got %q",
+							contentTypeFix, contentType)
+					}
+					b, err := ioutil.ReadAll(body)
+					if err != nil {
+						t.Fatal(err)
+					}
+					if str := string(b); str != reqBodyFix {
+						t.Errorf("wrong body, want %q, got %q",
+							reqBodyFix, str)
+					}
+					return tt.innerResponse, tt.innerError
+				},
+			}
+
+			sut := myhttp4.NewClient(inner, contentTypeFix)
+			got, err := sut.Post(urlFix, strings.NewReader(reqBodyFix))
+			if !equalError(err, tt.wantError) {
+				t.Errorf("wrong error: want %q, got %q", tt.wantError, err)
+			}
+			if got != tt.want {
+				t.Errorf("want %q, got %q", tt.want, got)
+			}
+		})
+	}
+}
