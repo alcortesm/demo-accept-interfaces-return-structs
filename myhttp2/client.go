05d42964f1e73735bc903daca97e24d83ba8fb04
Alberto Cortés <alcortesm@gmail.com>
2018-05-12 10:50:21 +0200

    myhttp2

---
+++ myhttp2/client.go
@@ -0,0 +1,49 @@
+package myhttp1
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func NewClient(timeout time.Duration, contentType string) *Client {
+	return &Client{
+		inner:       &http.Client{Timeout: timeout},
+		contentType: contentType,
+	}
+}
+
+type Client struct {
+	inner       *http.Client
+	contentType string
+}
+
+func (c *Client) Get(url string) (string, error) {
+	resp, err := c.inner.Get(url)
+	if err != nil {
+		return "", err
+	}
+	return readBody(resp)
+}
+
+func (c *Client) Post(url string, body io.Reader) (string, error) {
+	resp, err := c.inner.Post(url, c.contentType, body)
+	if err != nil {
+		return "", err
+	}
+	return readBody(resp)
+}
+
+func readBody(resp *http.Response) (string, error) {
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		err = errors.New(resp.Status)
+	}
+	return string(b), err
+}
