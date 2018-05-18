3a78210f9359fb6bf0ece4c0aa92ba6d73fc5b46
Alberto Cortés <alcortesm@gmail.com>
2018-05-18 19:49:28 +0200

    mymongo1: add a type as the DTO

--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 Quick way to setup a mongo server on localhost:
 
 ```
-docker run --name mymongo -d --rm -p 127.0.0.1:27017:27017 mongo:3.2.20-jessie
+; docker run --name mymongo -d --rm -p 127.0.0.1:27017:27017 mongo:3.2.20-jessie
+dca8c99a66589195a5838a1c8835f7ab73daeb1495af9105dcd65f9e0c06ae2c
 ```
 
 Quick way to open a mongo client to the server:
@@ -23,7 +24,7 @@ Questions? Try the support group
 	http://groups.google.com/group/mongodb-user
 2018-05-18T17:34:44.573+0000 I STORAGE  [main] In File::open(), ::open for '/home/mongodb/.mongorc.js' failed with errno:2 No such file or directory
 > db.abbreviations.find({})
-{ "_id" : ObjectId("5aff0ea842b7c6fe754d7a3f"), "abbreviation" : "NATO", "meaning" : "North Atlantic Treaty Organization" }
+{ "_id" : ObjectId("5aff0ea842b7c6fe754d7a3f"), "abbr" : "NATO", "data" : "North Atlantic Treaty Organization" }
 ```
 
 # References
--- mymongo1/mymongo.go
+++ mymongo1/mymongo.go
@@ -21,12 +21,16 @@ func NewDict(s *mgo.Session, d, c string) *Dict {
 	}
 }
 
+type entry struct {
+	ID           bson.ObjectId `bson:"_id,omitempty"`
+	Abbreviation string        `bson:"abbr"`
+	Meaning      string        `bson:"data"`
+}
+
 func (c *Dict) LookUp(a string) (string, error) {
 	col := c.session.Clone().DB(c.database).C(c.collection)
-	query := bson.M{"abbreviation": a}
-	var result struct {
-		Meaning string
-	}
+	query := bson.M{"abbr": a}
+	var result entry
 	if err := col.Find(query).One(&result); err != nil {
 		return "", fmt.Errorf("looking for %q: %s", a, err)
 	}
@@ -35,15 +39,12 @@ func (c *Dict) LookUp(a string) (string, error) {
 
 func (c *Dict) Add(a, m string) error {
 	col := c.session.Clone().DB(c.database).C(c.collection)
-	entry := struct {
-		Abbreviation string
-		Meaning      string
-	}{
+	doc := entry{
 		Abbreviation: a,
 		Meaning:      m,
 	}
-	if err := col.Insert(entry); err != nil {
-		return fmt.Errorf("inserting %q: %s", entry, err)
+	if err := col.Insert(doc); err != nil {
+		return fmt.Errorf("inserting %q: %s", doc, err)
 	}
 	return nil
 }
